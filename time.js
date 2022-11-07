b3af894cbd2cc97b20d44ff899f1e86fd82e7d7e
Artysuk <76599905+Artysuk@users.noreply.github.com>
2022-11-07 13:17:03 +0200

    Kell vahetatud

---
+++ time.js
@@ -0,0 +1,27 @@
+setInterval(displayTime, 1000);
+
+function displayTime() {
+
+    const timeNow = new Date();
+
+    let hoursOfDay = timeNow.getHours();
+    let minutes = timeNow.getMinutes();
+    let seconds = timeNow.getSeconds();
+    let weekDay = ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"]
+    let today = weekDay[timeNow.getDay()];
+    let months = timeNow.toLocaleDateString("ee", {
+        month: "long"
+    });
+
+    let year = timeNow.getFullYear();
+    
+    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hoursOfDay + ":" + minutes + ":" + seconds;
+
+    document.getElementById('Clock').innerHTML = time + " " + today + " " + months + " " + year;
+
+}
+displayTime();
